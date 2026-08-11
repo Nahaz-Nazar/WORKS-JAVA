80cbb4230709e388d823982f5ae9cacbef11966b
nahaz <nahaznazar73@gmail.com>
2026-08-11 08:54:04 +0530

    Add GreetingDisplay class to display a personalized greeting message

---
+++ ProfileSystem/src/greeting/GreetingDisplay.java
@@ -0,0 +1,10 @@
+package greeting;
+
+public class GreetingDisplay {
+
+    // Display the greeting message
+    public static void displayGreeting(String name) {
+
+        System.out.println("Welcome, " + name + "!");
+    }
+}
