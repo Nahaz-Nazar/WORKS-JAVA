88701fb1d740b94171730c02a82a3bfa44301481
nahaz <nahaznazar73@gmail.com>
2026-08-17 21:59:06 +0530

    Added ElectricScooter class implementing Vehicle interface with start and stop methods

---
+++ RentApp/src/rent/ElectricScooter.java
@@ -0,0 +1,15 @@
+package rent;
+
+
+public class ElectricScooter implements Vehicle {
+    
+    
+    public void start() {
+        System.out.println("Electric scooter powered on.");
+    }
+
+    
+    public void stop() {
+        System.out.println("Electric scooter powered off.");
+    }
+}
