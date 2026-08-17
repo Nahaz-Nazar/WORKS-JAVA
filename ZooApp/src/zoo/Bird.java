461d19f9c9db90ab6d46f4e7d3b72dda3ef0d2b0
nahaz <nahaznazar73@gmail.com>
2026-08-17 21:58:00 +0530

    Added Bird class implementing Animal interface with sound and movement methods

---
+++ ZooApp/src/zoo/Bird.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class Bird implements Animal {
+    
+    public void makeSound() { System.out.println("Chirp"); }
+    
+    public void move() { System.out.println("Fly"); }
+}
