e73650275fbbaf37e4df5086b43bdf2c39b11ea0
nahaz <nahaznazar73@gmail.com>
2026-08-17 21:58:10 +0530

    Added Dog class implementing Animal interface with sound and movement methods

---
+++ ZooApp/src/zoo/Dog.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class Dog implements Animal {
+    
+    public void makeSound() { System.out.println("Bark"); }
+   
+    public void move() { System.out.println("Run"); }
+}
